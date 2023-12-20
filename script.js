1e242d5fb3179b5759213a48b036f2ff171eb604
Your Name <your@email.com>
2023-12-20 22:02:58 +0100

    dom evant

--- script.js
+++ script.js
@@ -1,64 +1,27 @@
 'use strict';
-// console.log(document.querySelector('.message').textContent);
-let secnumber = Math.trunc(Math.random() * 20) + 1;
-// document.querySelector('.number').textContent = secnumber;
-let check = document.querySelector('.check');
-let Score = 20;
-let highscore = 0;
-let ScoreH = document.querySelector('.highscore');
-function displaymessage(messgae) {
-  document.querySelector('.message').textContent = messgae;
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+console.log(btnShowModal);
+const CloseModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+const AddModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', AddModal);
+  btnCloseModal.addEventListener('click', CloseModal);
+  overlay.addEventListener('click', CloseModal);
 }
-check.addEventListener('click', function () {
-  let guess = Number(document.querySelector('.guess').value);
-  //   console.log(guess, typeof guess);
-  if (!guess) {
-    console.log(!guess);
-    // document.querySelector('.message').textContent = 'No nember';
-    displaymessage('Please enter a number!');
-  } else if (guess === secnumber) {
-    // document.querySelector('.message').textContent = 'Correct! You win!';
-    displaymessage('Correct! You win!');
-    document.querySelector('body').style.backgroundColor = 'green';
-    document.querySelector('.number').textContent = secnumber;
-    // highscore = Score;
-
-    if (Score > highscore) {
-      highscore = Score;
-      document.querySelector('.highscore').textContent = highscore;
-    }
-  } else if (guess > secnumber) {
-    if (Score > 1) {
-      // document.querySelector('.message').textContent = 'Too high!';
-      displaymessage('Too high!');
-      Score = Score - 1;
-      document.querySelector('.score').textContent = Score;
-    } else {
-      // document.querySelector('.message').textContent = 'Too low!';
-      displaymessage('Too low!');
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('body').style.backgroundColor = 'red';
-    }
-  } else if (guess < secnumber) {
-    if (Score > 1) {
-      // document.querySelector('.message').textContent = 'Too low!';
-      displaymessage('Too low!');
-      Score = Score - 1;
-      document.querySelector('.score').textContent = Score;
-    } else {
-      // document.querySelector('.message').textContent = 'No! you lost !';
-      displaymessage('No! you lost !');
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('body').style.backgroundColor = 'red';
-      document.querySelector('.number').textContent = secnumber;
-    }
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Enter' && modal.classList.contains('hidden')) {
+    AddModal();
+  } else {
+    CloseModal();
   }
-});
-document.querySelector('.again').addEventListener('click', function () {
-  Score = 20;
-  document.querySelector('.score').textContent = Score;
-  secnumber = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.guess').value = '';
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').textContent = '?';
 });
